23c5853661773d4f04caf6fd9ea2d6361b944826
Aman Shakya <68556940+amanshakya99@users.noreply.github.com>
2025-09-19 12:11:42 +0900

    D

--- JS/app.js
+++
@@ -1,7 +0,0 @@
-const themeIcon = document.querySelector('.theme-icon');
-const body = document.body;
-const themeToggle = document.querySelector('.theme-toggle');
-themeIcon.addEventListner('click', () =>{
-    
-
-});
