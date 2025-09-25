f0b6940c8853146e590b2c7bb8c804cb9868a1f7
amanshakya99 <amanshakya99@gmail.com>
2025-09-25 14:23:18 +0900

    Navbar with Hero-Section

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+const themeIcon = document.querySelector('.theme-icon');
+const body = document.body;
+const themeToggle = document.querySelector('.theme-toggle');
+themeIcon.addEventListner('click', () =>{
+    
+
+});
